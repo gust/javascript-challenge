6b845aafecbea1441087bf4a265f5f028ea0f50e
For Glory <forglory09051@gmail.com>
2023-02-14 23:36:40 -0800

    load linkedCheckboxes widget

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
-import kjs           from './k';
-import drawers       from './widgets/drawers';
-import extendingForm from './widgets/extending-form';
-import tabs          from './widgets/tabs';
+import kjs              from './k';
+import drawers          from './widgets/drawers';
+import extendingForm    from './widgets/extending-form';
+import tabs             from './widgets/tabs';
+import linkedCheckboxes from './widgets/linked-checkboxes';
 
 document.addEventListener("DOMContentLoaded", () => {
-  kjs({ drawers, extendingForm, tabs }, document);
+  kjs({ drawers, extendingForm, tabs, linkedCheckboxes }, document);
 });
